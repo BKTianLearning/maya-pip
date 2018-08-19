ecef0a086f26402ef9cee8a6b5dc6ba6656928dd
Robert Joosten <rwm.joosten@gmail.com>
2018-08-19 14:22:08 +0100

    implement dummy linux query

--- src/mayapip/_internal/maya/__init__.py
+++ src/mayapip/_internal/maya/__init__.py
@@ -7,6 +7,8 @@ get_python_executables = None
 # get maya python executables based on platform
 if platform.system() == "Windows":
     from .maya_windows import get_python_executables
+elif platform.system() == "Linux":
+    from .maya_linux import get_python_executables
     
     
 def has_python_executables():
---
+++ src/mayapip/_internal/maya/maya_linux.py
@@ -0,0 +1,3 @@
+def get_python_executables():
+    maya_pythons = {}
+    return maya_pythons
